9ae6e612aa205eb39aac29f18cfc555a7f60549e
Yohei Okubo <okubo.yohei@gmail.com>
2015-08-05 20:00:02 +0900

    how handle immutable object?

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -1,7 +1,14 @@
 package controllers
 
+import org.joda.time.DateTime
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import slick._
+import play.db._
+
+import models.{UserHandle, User}
 
 /**
  * Created by bko on 8/5/15.
@@ -10,9 +17,27 @@ import play.api.mvc._
 
 class UsersController extends Controller {
 
-  def index = Action {
-    Ok(
-    <p>OKだよ</p>
+  var userDataForm: Form[User] = Form (
+    mapping(
+      "ID"         -> number,
+      "name"       -> nonEmptyText(maxLength = 10),
+      "email"      -> text, // TODO : validate
+      "password"   -> nonEmptyText(maxLength = 10), // TODO : digest
+      "created_at" -> jodaDate
+    )(User.apply)(User.unapply)
+  )
+
+  def showCreatePage = Action{
+    Ok(views.html.userIndex("user sing up"))
+  }
+
+  def create = Action { implicit request =>
+    userDataForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.userIndex(errors.toString)),
+      user => {
+        UserHandle.create(user)
+        Ok(views.html.userIndex(" SUCCESS!!! "))
+      }
     )
   }
 }
--- app/models/Tables.scala
+++ app/models/Tables.scala
@@ -1,6 +1,7 @@
 package models
 
 import org.joda.time.DateTime
+import org.joda.time._
 import slick.driver.H2Driver.api._
 import slick.lifted.{ProvenShape, ForeignKeyQuery}
 
@@ -9,61 +10,47 @@ import com.github.tototoshi.slick.H2JodaSupport._
 /**
  * Created by bko on 8/5/15.
  */
-trait Tables {
 
+
+
+
+
+case class User( ID:         Int,
+                 name:       String,
+                 email:      String,
+                 password:   String,
+                 created_at: DateTime )
+
+class UserTable(tag: Tag) extends Table[User](tag, "users") {
+  def ID         = column[Int]("id", O.PrimaryKey, O.AutoInc)
+  def name       = column[String]("name")
+  def password   = column[String]("password")
+  def email      = column[String]("email")
+  def created_at = column[DateTime]("CreatedAt")
+
+  def * = (ID, name, password, email, created_at) <> (User.tupled, User.unapply)
 }
 
+object UserHandle {
+  lazy val userQuery = TableQuery[UserTable]
 
-// A Suppliers table with 6 columns: id, name, street, city, state, zip
-class Suppliers(tag: Tag)
-  extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-
-  // This is the primary key column:
-  def id: Rep[Int] = column[Int]("SUP_ID", O.PrimaryKey)
-  def name: Rep[String] = column[String]("SUP_NAME")
-  def street: Rep[String] = column[String]("STREET")
-  def city: Rep[String] = column[String]("CITY")
-  def state: Rep[String] = column[String]("STATE")
-  def zip: Rep[String] = column[String]("ZIP")
-
-  // Every table needs a * projection with the same type as the table's type parameter
-  def * : ProvenShape[(Int, String, String, String, String, String)] =
-    (id, name, street, city, state, zip)
+  def create(user: User) {
+    userQuery.insertOrUpdate(  )
+  }
+//
+//  /**
+//   * 更新
+//   * @param customer
+//   */
+//  def update(customer: Customer)(implicit s: Session) {
+//    customerQuery.filter(_.ID === customer.ID).update(customer)
+//  }
+//
+//  /**
+//   * 削除
+//   * @param customer
+//   */
+//  def remove(customer: Customer)(implicit s: Session) {
+//    customerQuery.filter(_.ID === customer.ID).delete
+//  }
 }
-
-// A Coffees table with 5 columns: name, supplier id, price, sales, total
-class Coffees(tag: Tag)
-  extends Table[(String, Int, Double, Int, Int)](tag, "COFFEES") {
-
-  def name: Rep[String] = column[String]("COF_NAME", O.PrimaryKey)
-  def supID: Rep[Int] = column[Int]("SUP_ID")
-  def price: Rep[Double] = column[Double]("PRICE")
-  def sales: Rep[Int] = column[Int]("SALES")
-  def total: Rep[Int] = column[Int]("TOTAL")
-
-  def * : ProvenShape[(String, Int, Double, Int, Int)] =
-    (name, supID, price, sales, total)
-
-  // A reified foreign key relation that can be navigated to create a join
-  def supplier: ForeignKeyQuery[Suppliers, (Int, String, String, String, String, String)] =
-    foreignKey("SUP_FK", supID, TableQuery[Suppliers])(_.id)
-}
-
-
-class Users(tag: Tag)
-  extends Table[(Int, String, String, String, DateTime)](tag, "USERS") {
-
-  // This is the primary key column:
-  def id: Rep[Int] = column[Int]("USER_ID", O.PrimaryKey) //0
-  def name: Rep[String] = column[String]("USER_NAME") //1
-  def mail: Rep[String] = column[String]("MAIL") //2
-  def password_digest: Rep[String] = column[String]("PASSWORD")
-  def created_at: Rep[DateTime]  = column[DateTime]("CREATED_AT") //3
-
-
-  // Every table needs a * projection with the same type as the table's type parameter
-  def * : ProvenShape[(Int, String, String, String, DateTime)] = (id, name, mail, password_digest, created_at)
-}
-
-
-
